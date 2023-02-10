ce7991a523564095ba3afd32142a3208dc968cdf
Shamith <shamith301102@gmail.com>
2023-02-11 01:01:27 +0530

    Create To_get_the_largest_number_from_the_list_(Lists).py

---
+++ To_get_the_largest_number_from_the_list_(Lists).py
@@ -0,0 +1,7 @@
+# Problem Statement: To get the largest number from the list (Lists)
+#
+# Sample Input 0: 15 8 99 6 13
+# Sample Output 0: 99 
+
+# My Solution: ( Maybe the best solution? )
+print(max(list(map(int,input().split()))))
