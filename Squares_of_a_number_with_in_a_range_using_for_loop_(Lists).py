f4dab208aa677b17c1633e662ada3ecaa3d41a6a
Shamith <shamith301102@gmail.com>
2023-02-11 12:52:20 +0530

    Create Squares_of_a_number_with_in_a_range_using_for_loop_(Lists).py

---
+++ Squares_of_a_number_with_in_a_range_using_for_loop_(Lists).py
@@ -0,0 +1,9 @@
+# Problem Statement: Program to print squares of a number with in a range.
+#
+# Constraints: Input > 0
+#
+# Sample Input 0: 5
+# Sample Output 0: [1, 4, 9, 16, 25]
+    
+# My Solution:
+print( [ i**2 for i in range(1,int(input())+1)] )
