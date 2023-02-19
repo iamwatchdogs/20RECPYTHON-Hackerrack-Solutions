7964b205efdcfa331003f344d684672fb17c7135
Shamith <shamith301102@gmail.com>
2023-02-19 20:00:05 +0530

    Create Conversion_of_characters(lists).py

---
+++ Conversion_of_characters(lists).py
@@ -0,0 +1,10 @@
+# Problem Statement: Arjun has set of items and he wants to combine items into a single unit.
+#
+# Sample Input 0: a b c d
+# Sample Output 0: abcd
+
+# My Solution:
+print(input().replace(' ',''))
+
+# Alternative Solution:
+print(''.join(input().split()))
